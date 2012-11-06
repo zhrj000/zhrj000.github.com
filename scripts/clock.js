16b5056aaa2884a4196fc52e30fbd035a5b37a14
zhrj000 <zhrj000@163.com>
2012-11-06 19:54:22 +0800

    1

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,10 +1,10 @@
 (function(){
 	var timeId=setTimeout(function (){
 		
-			var pointer=Mixiu.getElementsByClassName(document,"pointer")[0],
-				hour=Mixiu.getElementsByClassName(pointer,"hour")[0],
-				second=Mixiu.getElementsByClassName(pointer,"second")[0],
-				minute=Mixiu.getElementsByClassName(pointer,"minute")[0],
+			var pointer=Mixiu.ClassUtil.getElementsByClassName(document,"pointer")[0],
+				hour=Mixiu.ClassUtil.getElementsByClassName(pointer,"hour")[0],
+				second=Mixiu.ClassUtil.getElementsByClassName(pointer,"second")[0],
+				minute=Mixiu.ClassUtil.getElementsByClassName(pointer,"minute")[0],
 			hours=-new Date().getHours()*360/12-new Date().getMinutes()*360/720-new Date().getSeconds()*360/(12*60*60)-90,
 			minutes=-new Date().getMinutes()*360/60-new Date().getSeconds()*360/3600-90,
 			seconds=-new Date().getSeconds()*360/60-90;
