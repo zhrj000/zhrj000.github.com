f0f22a5df708594e35a7363417a59125eac9d246
zhrj000 <zhrj000@163.com>
2012-11-22 14:04:37 +0800

    1

---
+++ scripts/canvas.js
@@ -0,0 +1,202 @@
+//(function(){
+
+	//画成绩分析折线图
+	function drawBrokenLine(scores,name){
+		var canvas=document.getElementById("canvas"),
+			ctx=canvas.getContext('2d');
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		drawCoordinate(ctx,10,290,470,240,"#fff");
+
+		var count=scores.length,  //分数个数
+			pointL=240/100,       //每分长度
+			boutL,                //每次长度
+			x=[],
+			y=[];
+		if(count<=10){
+			boutL=45;
+		}else{
+			boutL=450/count;
+		}
+		drawText(ctx,10,20,name+"同学的成绩折线图","#FFF");
+
+		//画一根及格线
+		drawLine(ctx,10,290-pointL*60,460,290-pointL*60,1,"#FF0000");
+		drawText(ctx,10,290-pointL*60,"pass line","#FF0000");
+		//画分数线
+		for(var i=0;i<count;i++){
+			x[i]=(i+1)*boutL+10,
+			y[i]=290-scores[i]*pointL;
+			drawCircle(ctx,x[i],y[i],4,"#49B4F2");
+			drawText(ctx,x[i],y[i],scores[i],"#fff");
+			if(i!==0){
+				if(y[i]>y[i-1]){
+					drawLine(ctx,x[i],y[i],x[i-1],y[i-1],1,"#49B4F2");
+				}else{
+					drawLine(ctx,x[i],y[i],x[i-1],y[i-1],1,"#49B4F2");
+				}
+				
+			}
+		}
+	}
+
+	//画饼状图
+	function drawPieChart(scores,name){
+		var canvas=document.getElementById("canvas"),
+			ctx=canvas.getContext('2d'),
+			level=[{count:0,text:"90~100",colors:"#8FB5EB"},
+				   {count:0,text:"80~90",colors:"#8FEB90"},
+				   {count:0,text:"70~80",colors:"#FFEB9B"},
+				   {count:0,text:"60~70",colors:"#CC8FEB"},
+				   {count:0,text:"0~60",colors:"#FF9B9B"}],
+			slength=scores.length,
+			beginrad=0,
+			endrad=0;
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		drawText(ctx,10,20,name+"的成绩分布饼状图","#FFF");
+		for(var i=slength-1;i>=0;i--){
+			if(scores[i]>=90){
+				level[0].count++;
+			}else if(scores[i]>=80){
+				level[1].count++;
+			}else if(scores[i]>=70){
+				level[2].count++;
+			}else if(scores[i]>=60){
+				level[3].count++;
+			}else{
+				level[4].count++;
+			}
+		}
+		for(i=4;i>=0;i--){			
+			endrad=level[i].count/slength*360+beginrad;
+			drawFan(ctx,150,150,100,beginrad,endrad,level[i].colors);
+			if((endrad-beginrad)!==0){
+				console.log((endrad+beginrad)/2);
+				console.log(150+50*Math.cos((endrad+beginrad)/2*Math.PI/180));
+				console.log(150+50*Math.sin((endrad+beginrad)/2*Math.PI/180));
+				drawText(ctx,150+50*Math.cos((endrad+beginrad)/2*Math.PI/180)-10,150+50*Math.sin((endrad+beginrad)/2*Math.PI/180),Math.round((level[i].count/slength)*1000)/10+"%","#000");
+			}
+			beginrad=endrad;
+			drawRectangle(ctx,400,30*(i+1),15,15,level[i].colors);
+			drawText(ctx,420,30*(i+1)+14,level[i].text,"#fff");
+		}
+
+
+	}
+
+	//画长方形
+	function drawRectangle(ctx,x,y,width,height,color){
+		ctx.fillStyle=color;				
+		ctx.beginPath();
+		ctx.moveTo(x,y);
+		ctx.lineTo(x+width,y);
+		ctx.lineTo(x+width,y+height);
+		ctx.lineTo(x,y+height);
+		ctx.closePath();
+		ctx.fill();
+	}
+	//画扇形
+	function drawFan(ctx,x,y,length,radB,radE,color){
+		if(radE-radB>180){
+			drawFan(ctx,x,y,length,radB,radB+180,color);
+			drawFan(ctx,x,y,length,radB+180,radE,color);
+		}else{
+			ctx.fillStyle=color;
+			ctx.beginPath();
+			ctx.arc(x,y,length,Math.PI/180*radB,Math.PI/180*radE);
+			ctx.closePath();
+			ctx.fill();
+			drawIsoscelesTriangle(ctx,x,y,length,radB,radE,color);
+		}
+	}
+	//绘制文本
+	function drawText(ctx,x,y,text,color){
+		ctx.font="14px Arial";
+		ctx.fillStyle=color;
+		ctx.fillText(text,x,y);
+	}
+
+	//画直线
+	function drawLine(ctx,xb,yb,xe,ye,thickness,color){
+		ctx.beginPath();
+		ctx.moveTo(xb,yb);
+		ctx.lineTo(xe,ye);
+		ctx.lineWidth=thickness+"px";
+		ctx.strokeStyle=color;
+		ctx.closePath();
+		ctx.stroke();
+	}
+
+	//画圆
+	function drawCircle(ctx,x,y,radius,color){
+		ctx.fillStyle=color;
+		ctx.beginPath();
+		ctx.arc(x,y,radius,0,Math.PI*2,true);
+		ctx.closePath();
+		ctx.fill();
+	}
+
+	//添加三角形
+	// function addTringle(ctx,x,y,length,num,color){
+	// 	var rad=num*Math.PI/180;
+	// 	ctx.rotate(rad);
+	// 	if(num%180===0){
+	// 		ctx.translate(-(x+length/2),-y);
+	// 		drawTriangle(ctx,x,y,length,color);
+	// 		ctx.translate((x+length/2),y);
+	// 	}else if(num%270===0){
+	// 		ctx.translate(-(y+length/2),(x-length/2));
+	// 		drawTriangle(ctx,x,y,length,color);
+	// 		ctx.translate((y+length/2),-(x-length/2));
+	// 	}
+	// 	ctx.rotate(-rad);				
+	// }
+
+	// //画三角形
+	// function drawTriangle(ctx,x,y,length,color){		
+	// 	ctx.fillStyle=color;				
+	// 	ctx.beginPath();
+	// 	ctx.moveTo(0,0);
+	// 	ctx.lineTo(length,0);
+	// 	ctx.lineTo((length/2),length*Math.sin(Math.PI/3));
+	// 	ctx.lineTo(0,0);
+	// 	ctx.closePath();
+	// 	ctx.fill();
+	// } 
+
+
+	//画一个等腰三角形，起点，腰长，开始角度，结束角度，颜色
+	function drawIsoscelesTriangle(ctx,x,y,length,radB,radE,color){
+		radB=radB*Math.PI/180;
+		radE=radE*Math.PI/180;
+		ctx.fillStyle=color;
+		ctx.beginPath();
+		ctx.moveTo(x,y);
+		ctx.lineTo(x+length*Math.cos(radB),y+length*Math.sin(radB));
+		ctx.lineTo(x+length*Math.cos(radE),y+length*Math.sin(radE));
+		ctx.closePath();
+		ctx.fill();
+	}
+	//画二维直角坐标系
+	function drawCoordinate(ctx,originX,originY,lengthX,lengthY,color){
+		drawLine(ctx,originX,originY,originX,originY-lengthY,1,color);
+		drawLine(ctx,originX,originY,lengthX-originX,originY,1,color);
+		drawIsoscelesTriangle(ctx,originX,originY-lengthY,10,60,120,color);
+		drawIsoscelesTriangle(ctx,lengthX-originX,originY,10,150,210,color);
+		
+		// addTringle(ctx,originX,originY-lengthY,10,180,color);
+		// addTringle(ctx,lengthX-originX,originY,10,270,color);
+	}
+
+// 	var scores=[90,89,55,76,88,83,100,90,91,92,91.5];
+// 	drawBrokenLine(scores);
+// 	var canvas=document.getElementById("canvas"),
+// 			ctx=canvas.getContext('2d');
+// 	ctx.clearRect(0,0,canvas.width,canvas.height);
+// 	drawPieChart();
+// })();
+
+
+/*
+  关于canvas的变形，其实将canvas看成一个不透明的画布，上面可以画东西，而context则是一个透明的画布的感觉，
+  大小与canvas一样大小，原点与canvas的原点重合，在canvas上的所有变形如旋转或者移动都是context的操作
+ */
---
+++ scripts/scoresanalysis.js
@@ -0,0 +1,397 @@
+(function(){
+	var datatable=null,
+		db=openDatabase('MyData','','My Database',1024*1024*2),
+		EventUtil={
+			addHandler:function(element,type,handdler){
+				if(element.addEventListener){
+					element.addEventListener(type,handdler,false);
+				}else if(element.attachEvent){
+					element.attachEvent("on"+type,handdler);
+				}else{
+					element["on"+type]=handdler;
+				}
+			},
+			removeHandler:function(element,type,handdler){
+				if(element.removeEventListener){
+					element.removeEventListener(type,handdler,false);
+				}else if(element.detachEvent){
+					element.detachEvent("on"+type,handdler);
+				}else{
+					element["on"+type]=null;
+				}
+			},
+			getEvent:function(event){
+				return event?event:window.event;
+			},
+			getTarget:function(event){
+				return event.target||event.srcElement;
+			}
+		},
+		ClassUtil={
+			hasClass:function(node,classname){
+				var classnames=node.className.split(' '),
+					j;
+				for(j=0;j<classnames.length;j++){
+					if(classname===classnames[j]){
+						return true;
+					}
+				}
+				return false;
+			},
+			addClass:function(node,classname){
+				var classnames=node.className.split(' '),
+					j;
+				for(j=0;j<classnames.length;j++){
+					if(classname===classnames[j]){
+						break;
+					}
+				}
+				if(j===classnames.length){
+					node.className+=(" "+classname);
+				}
+			},
+			removeClass:function(node,classname){
+				var classnames=node.className.split(' '),
+					j;
+				for(j=0;j<classnames.length;j++){
+					if(classname===classnames[j]){
+						if(0===j){
+							node.className=node.className.toString().replace(classname,'');
+						}else{
+							node.className=node.className.toString().replace(' '+classname,'');
+						}
+				
+					}
+				}
+			}
+		};
+	function init(){
+		if(db===null)alert("yes");
+		datatable=document.getElementById("datatable");
+		showAllData();
+		EventUtil.addHandler(document,"click",function(event){
+			event=EventUtil.getEvent(event);
+			var target=EventUtil.getTarget(event);
+			switch(target.id){
+				case "savestu":
+					saveStuInfo();
+					break;
+				case "savetran":
+					saveTranInfo();
+					break;
+				case "saveall":
+					saveAll();
+					break;
+				case "delstu":
+					delStuInfo();
+					break;
+				case "deltran":
+					delTranInfo();
+					break;
+			}
+		});
+		EventUtil.addHandler(datatable,"click",function(event){
+			event=EventUtil.getEvent(event);
+			var target=EventUtil.getTarget(event);
+			if(ClassUtil.hasClass(target,"clickable")){
+				ClassUtil.addClass(target,"read-write");
+			}
+		});
+		EventUtil.addHandler(document,"mouseover",function(event){
+			event=EventUtil.getEvent(event);
+			var target=EventUtil.getTarget(event);
+			if(ClassUtil.hasClass(target,"viewanaly")){
+				var x=getElementLeft(target),
+					y=getElementTop(target)-document.getElementById("canvas").height-22;
+				showCanvascard(target,x,y);
+			}else if(ClassUtil.hasClass(target,"transcripts_name")){
+				var x=getElementLeft(target),
+					y=getElementTop(target)-document.getElementById("canvas").height-22;
+				showCanvascard(target,x,y);
+			}else if(target.id==="canvascard"||target.id==="canvas"){
+
+			}else{
+				var canvascard=document.getElementById("canvascard");
+				canvascard.style.display="none";
+			}
+		});
+	}
+
+	function showCanvascard(element,x,y){
+		var tr=element.parentNode,
+			ths=tr.childNodes,
+			scores=[],
+			name,
+			canvascard=document.getElementById("canvascard");
+		canvascard.style.top=y+"px";
+		canvascard.style.left=x+"px";
+		//alert(canvascard.style.top+","+canvascard.style.left);
+		canvascard.style.display="block";
+		if(ClassUtil.hasClass(element,"viewanaly")){
+			for(var i=0,max=ths.length;i<max;i++){
+				if(ClassUtil.hasClass(ths[i],"stu_score")){
+					scores[scores.length]=Number(ths[i].innerText);
+				}else if(ClassUtil.hasClass(ths[i],"stu_name")){
+					name=ths[i].innerText;
+				}
+			}
+	 		drawBrokenLine(scores,name);
+		}else if(ClassUtil.hasClass(element,"transcripts_name")){
+			var tranid=element.getAttribute("data-tranid"),
+				tds=document.getElementsByClassName("stu_score");
+			name=element.innerText;
+			for(var i=0,max=tds.length;i<max;i++){
+				if(tds[i].getAttribute("data-tranid")===tranid){
+					scores[scores.length]=Number(tds[i].innerText);
+				}
+			}
+			drawPieChart(scores,name);
+		}
+		
+	}
+	//剩下所有的数据除了表头剩下
+	function removeAllData(){
+		//去掉datatable的所有子节点
+		for(var i=datatable.childNodes.length-1;i>=0;i--){
+			datatable.removeChild(datatable.childNodes[i]);
+		}
+		var tr=document.createElement('tr'),
+			th2=document.createElement('th'),
+			th3=document.createElement('th');
+		th2.innerHTML="学号";
+		th3.innerHTML="姓名";
+		tr.appendChild(th2);
+		tr.appendChild(th3);
+		//取出成绩单表头
+		db.transaction(function(tx){
+			tx.executeSql('SELECT * FROM TranInfo',[],function(tx,rs){
+				for(var i=0;i<rs.rows.length;i++){
+					var th=document.createElement('th'),
+						input=document.createElement('input');
+					th.innerText=rs.rows.item(i).transcripts;
+					th.setAttribute("data-tranid",rs.rows.item(i).tranid);
+					ClassUtil.addClass(th,"transcripts_name");
+					ClassUtil.addClass(th,"clickable");
+					tr.appendChild(th);
+				}
+			});
+		});
+		datatable.appendChild(tr);
+	}
+
+	//row是学生信息
+	function showData(row){
+		var tr=document.createElement('tr'),
+			td2=document.createElement('td'),
+			td3=document.createElement('td');
+		td2.innerHTML=row.stuid;
+		td2.className="viewanaly";
+		td3.innerHTML=row.name;
+		td3.setAttribute("data-stuid",row.stuid);
+		ClassUtil.addClass(td3,"stu_name");
+		ClassUtil.addClass(td3,"clickable");
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		//取出每个学生信息的同时取出相应的所有成绩
+		db.transaction(function(tx){
+			var transcripts=datatable.getElementsByClassName("transcripts_name");
+			for(var i=0;i<transcripts.length;i++){
+				(function(num){
+					var tranid=transcripts[num].getAttribute("data-tranid");
+				tx.executeSql('SELECT scores FROM ScoresInfo WHERE stuid=? AND tranid=?',[row.stuid,tranid],function(tx,rs){
+					var td=document.createElement('td');
+					td.setAttribute("data-stuid",row.stuid);
+					td.setAttribute("data-tranid",tranid);
+					ClassUtil.addClass(td,"stu_score");
+					ClassUtil.addClass(td,"clickable");
+					tr.appendChild(td);
+					//alert(row.stuid+","+tranid);
+					//alert(rs.rows.item.length);
+					if(rs.rows.length===0){
+						tx.executeSql('INSERT INTO ScoresInfo VALUES(?,?,?)',[td.getAttribute("data-stuid"),td.getAttribute("data-tranid"),0]);
+						td.innerHTML="0";
+					}else{
+					 	td.innerHTML=rs.rows.item(0).scores;
+					}
+					
+				},function(tx,error){
+					//alert(error.source+"::"+error.message);
+				});
+				})(i);
+				
+			}
+			
+		});
+		datatable.appendChild(tr);
+	}
+
+	//取出所有数据
+	function showAllData(){
+		db.transaction(function(tx){
+			//如果3个表都不存在则创建
+			tx.executeSql('CREATE TABLE IF NOT EXISTS StuInfo(stuid unique,name TEXT)',[]);
+			tx.executeSql('CREATE TABLE IF NOT EXISTS TranInfo(tranid unique,transcripts TEXT)',[]);
+			tx.executeSql('CREATE TABLE IF NOT EXISTS ScoresInfo(stuid INTEGER,tranid TEXT,scores REAL)',[]);
+			
+			tx.executeSql('SELECT * FROM StuInfo',[],function(tx,rs){
+
+				//取出所有数据之前先清除掉所有已经存在的数据
+				removeAllData();
+				
+				for(var i=0;i<rs.rows.length;i++){
+					showData(rs.rows.item(i));
+				}
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+	}
+	function addStuInfo(stuid,name){
+		db.transaction(function(tx){
+			//alert("tx:"+tx);
+			tx.executeSql(' INSERT  INTO  StuInfo  VALUES(?, ?)',[stuid,name],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+	}
+	function delStuInfo(){
+		var stuid=document.getElementById("stuid").value,
+			name=document.getElementById("name").value;
+		db.transaction(function(tx){
+			//alert("tx:"+tx);
+			tx.executeSql(' DELETE  FROM  StuInfo  WHERE stuid=?',[stuid],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+		document.getElementById("stuid").value="";
+		document.getElementById("name").value="";
+		showAllData();
+	}
+	function updateStuInfo(stuid,name){
+		db.transaction(function(tx){
+			tx.executeSql('UPDATE StuInfo set name=?  WHERE stuid=?;',[name,stuid],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+	}
+	function saveStuInfo(){
+		var stuid=document.getElementById("stuid").value,
+			name=document.getElementById("name").value;
+		addStuInfo(stuid,name);
+		document.getElementById("stuid").value="";
+		document.getElementById("name").value="";
+		showAllData();
+	}
+	function addTranInfo(tranid,transcripts){
+		db.transaction(function(tx){
+			tx.executeSql('INSERT INTO TranInfo VALUES(?,?)',[tranid,transcripts],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+	}
+	function delTranInfo(){
+		var transcripts=document.getElementById("transcripts").value,
+			time=new Date().getTime().toString();
+		db.transaction(function(tx){
+			//alert("tx:"+tx);
+			tx.executeSql(' DELETE  FROM  TranInfo  WHERE transcripts=?',[transcripts],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+		document.getElementById("transcripts").value="";
+		showAllData();
+	}
+	function updateTranInfo(tranid,transcripts){
+		db.transaction(function(tx){
+			tx.executeSql('UPDATE TranInfo set transcripts=?  WHERE tranid=?;',[transcripts,tranid],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+	}
+	function saveTranInfo(){
+		var transcripts=document.getElementById("transcripts").value,
+			time=new Date().getTime().toString();
+		addTranInfo(time,transcripts);
+		document.getElementById("transcripts").value="";
+		showAllData();
+	}
+	function updateScoresInfo(stuid,tranid,scores){
+		db.transaction(function(tx){
+			tx.executeSql('UPDATE ScoresInfo set scores=?  WHERE stuid=? AND tranid=?;',[scores,stuid,tranid],function(tx,rs){
+
+			},function(tx,error){
+				alert(error.source+"::"+error.message);
+			});
+		});
+	}
+	function saveAll(){
+		var change=datatable.getElementsByClassName("read-write");
+		for(var i=change.length-1;i>=0;i--){
+			if(ClassUtil.hasClass(change[i],"transcripts_name")){
+				var tranid=change[i].getAttribute("data-tranid"),
+					transcripts=change[i].innerText.toString();
+				//alert("tranid:"+tranid+",transcripts:"+transcripts);
+				updateTranInfo(tranid,transcripts);
+				ClassUtil.removeClass(change[i],"read-write");
+			}else if(ClassUtil.hasClass(change[i],"stu_name")){
+				var stuid=change[i].getAttribute("data-stuid"),
+					name=change[i].innerText.toString();
+				updateStuInfo(stuid,name);
+				ClassUtil.removeClass(change[i],"read-write");
+			}else if(ClassUtil.hasClass(change[i],"stu_score")){
+				var stuid=change[i].getAttribute("data-stuid"),
+					tranid=change[i].getAttribute("data-tranid"),
+					scores=change[i].innerText.toString();
+				updateScoresInfo(stuid,tranid,scores);
+				ClassUtil.removeClass(change[i],"read-write");
+			}
+		}
+	}
+	// window.onload=function(){
+		
+	// }
+
+	function getElementLeft(element){
+　　　　var actualLeft = element.offsetLeft;
+　　　　var current = element.offsetParent;
+
+　　　　while (current !== null){
+　　　　　　actualLeft += current.offsetLeft;
+　　　　　　current = current.offsetParent;
+　　　　}
+
+　　　　return actualLeft;
+　　}
+
+　　function getElementTop(element){
+　　　　var actualTop = element.offsetTop;
+　　　　var current = element.offsetParent;
+
+　　　　while (current !== null){
+　　　　　　actualTop += current.offsetTop;
+　　　　　　current = current.offsetParent;
+　　　　}
+
+　　　　return actualTop;
+　　}
+	EventUtil.addHandler(window,"load",function(){
+		init();
+	});
+
+
+})();
+
+
+//数据库地址：C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default\databases
